9a1954487cc0a26233d104b012a30ab36f5b907b
nathvng <nathaliagabrielevenegasiqueira@gmail.com>
2025-09-04 15:38:12 -0300

    backend arrumado com alteração no tipo maven

---
+++ faceMouse/faceMouse/src/main/java/com/facemouse/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.facemouse.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.facemouse.entities.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
+
+
+
